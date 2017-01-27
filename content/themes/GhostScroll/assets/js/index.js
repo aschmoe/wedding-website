6a4e433f1321445361eec60fbc3f30b5d8953700
Alex Schmoe <aschmoe@gmail.com>
2017-01-26 22:42:35 -0800

    stashing blue

--- content/themes/GhostScroll/assets/js/index.js
+++ content/themes/GhostScroll/assets/js/index.js
@@ -99,6 +99,64 @@ var $sitehead = $('#site-head');
 		$('ul li').before('<span class="bult fa fa-asterisk icon-asterisk"></span>');
 		$('blockquote p').prepend('<span class="quo icon-quote-left"></span>');
 		$('blockquote p').append('<span class="quo icon-quote-right"></span>');
+
+		// Blend 'em
+		// $('#backdrop').one('load', function() {
+		//   // do stuff
+		// }).each(function() {
+		//   if(this.complete) {
+		//   	
+		//   }
+		// });
+
+		var blendImages = {
+			'backdrop': {
+				loaded: false,
+				src: "https://s3-us-west-2.amazonaws.com/schmolga-wedding/natural-paper-background-texture-hd-5a0b-2.jpg"
+			},
+			'alex-image': {
+				loaded: false,
+				src: "https://s3-us-west-2.amazonaws.com/schmolga-wedding/alex-cutout.png"
+			},
+			'olga-image': {
+				loaded: false,
+				src: "https://s3-us-west-2.amazonaws.com/schmolga-wedding/olga-cutout.png"
+			}
+		};
+		var run1 = false;
+		var imageLoaded = function() {
+			var key = this.attributes.id;
+			if(blendImages[key].canvas) {
+				return;
+			}
+			var canvas = document.createElement("canvas"),
+					ctx = canvas.getContext("2d");
+			canvas.width = this.width;
+	    canvas.height = this.height;
+	    ctx.drawImage( this, 0, 0 );
+	    var imgSrc = canvas.toDataURL("image/png");
+	    // localStorage.setItem( key, imgSrc );
+	    blendImages[key].image.src =  imgSrc;
+      blendImages[key].loaded = true;
+      blendImages[key].canvas = canvas;
+      if(blendImages['backdrop'].loaded && blendImages['alex-image'].loaded && blendImages['olga-image'].loaded) {
+      	var $backdrop = $(blendImages['backdrop'].canvas); 
+      	$('#site-head-content').append($backdrop);
+      	// $backdrop.blendmode({'mode' : 'hardlight', 'object' : $(blendImages['alex-image'].image), 'scaletofit' : false});
+      	run1 = true;
+      }
+    };
+   //  Object.keys(blendImages).map(function(key) {
+			// var tmpImg = new Image();
+			// tmpImg.crossOrigin = "Anonymous";
+   //    tmpImg.onload = imageLoaded;
+   //    tmpImg.attributes.id = key;
+   //    tmpImg.src = blendImages[key].src;
+   //    blendImages[key].image = tmpImg;
+   //  })
+    
+// }) ;
+		// $('#sourceimage').blendmode({'mode' : modePresets.options[i].value, 'object' : $('#blendimage'), 'scaletofit' : scale});
 	});
 	
 	$post.each(function () {
@@ -119,20 +177,64 @@ var $sitehead = $('#site-head');
 		}
 	});
 
+	// // SVGs
+ //  var imgURL = 'https://s3-us-west-2.amazonaws.com/schmolga-wedding/adaptive-border.svg';
+ //  $.get(imgURL, function(data) {
+ //  	$('.flourish').each(function(){
+ //    	var $img = $(this);
+ //      // Get the SVG tag, ignore the rest
+ //      var $svg = $(data).find('svg');
+ //      // Remove any invalid XML tags as per http://validator.w3.org
+ //      $svg = $svg.removeAttr('xmlns:a');
+ //      // Replace image with new SVG
+ //      $img.append($svg);
+ //     });
+ //  }, 'xml');
+
+  // Canvas background
+  
+  // <img id="olga-image" src="https://s3-us-west-2.amazonaws.com/schmolga-wedding/olga-cutout.png" />
+  // <img id="alex-image" src="https://s3-us-west-2.amazonaws.com/schmolga-wedding/alex-cutout.png" />
+
+ //  var canvas = document.getElementById('canvas');
+	// var ctx = canvas.getContext('2d');
+	// var imgback = document.createElement('img');
+	// imgback.setAttribute('id', 'background');
+	// imgback.src = "https://s3-us-west-2.amazonaws.com/schmolga-wedding/natural-paper-background-texture-hd-5a0b-2.jpg";
+	// var imgalex = document.createElement('img');
+	// imgalex.setAttribute('id', 'alex-image');
+	// imgalex.src = 'https://s3-us-west-2.amazonaws.com/schmolga-wedding/alex-cutout.png';
+	// var imgolga = document.createElement('img');
+	// imgolga.setAttribute('id', 'olga-image');
+	// imgolga.src = 'https://s3-us-west-2.amazonaws.com/schmolga-wedding/olga-cutout.png';
+
+	// // ctx.globalCompositeOperation = 'source-over';
+	// ctx.globalCompositeOperation = 'hard-light';
+	// ctx.drawImage(imgback, 0, 0);
+	// ctx.drawImage(imgalex, 0, 0);
+	// ctx.drawImage(imgolga, 0, 0);
+
 	function thanksForm() {
-		var label = $('#bartend > .form-label:first-child');
+		var label = $('#bartend > p:first-child');
 		label.siblings().hide();
-		label.after('<p>Thanks for your input!</p>');
+		label.after('<p>Thanks for the information, talk to you soon!</p>');
 	}
-	
+
+	// //
 	$('#bartend-submit').click(function(e) {
-		console.log('yolo');
-		var var1 = $('input[name="optionsRadios"]').val(),
-		    var2 = $('textarea[name="message"]').val();
+		var var1 = $('input[name="optionsRadios"]:checked').val(),
+		    var2 = $('textarea[name="message"]').val(),
+		    var3 = $('input[name="name"]').val(),
+		    var4 = $('textarea[name="address"]').val();
 		e.preventDefault();
 		$.ajax({
         url: "https://script.google.com/macros/s/AKfycbwL5Qrk35H76B4RJK0OQO5yzI0ZEFqk8uCauTTopyyB1hT-fWw/exec",
-        data: {"Sign up for a 30 minute shift as bartender with a partner?" : var1, "Comments" : var2 },
+        data: {
+        	"Would you sign up for a 30 minute shift as bartender with a partner?" : var1, 
+        	"Anything else?" : var2,
+        	"What is your name?": var3,
+        	"What is your mailing address?": var4
+        },
         type: "POST",
         dataType: "xml",
         statusCode: {
