3286e97ff464b4996d0b2f2d0ad12764f527a56e
Alex Schmoe <aschmoe@gmail.com>
2017-07-19 13:52:16 -0700

    adding bartenders

--- content/themes/GhostScroll/assets/js/bartenders.js
+++ content/themes/GhostScroll/assets/js/bartenders.js
@@ -1,73 +1,10 @@
-firebase.auth().signInAnonymously().catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log(error);
-});
-
 /* Globals jQuery, document */
 (function ($) {
 
-  var contactsList = {};
-
-  // Firebase
-  // -----------------------
-
-  //create firebase reference
-  var contactsRef = firebase.database().ref();
-
-  //load older conatcts as well as any newly added one...
-  contactsRef.on('child_added', function(snap) {
-    console.log('added', snap.key, snap.val());
-    var val = snap.val();
-    contactsList[snap.key] = {
-      value: val,
-      html: contactFromObject(val, snap.key)
-    };
-    addToBucket(snap.key);
-  });
-
-  //load older conatcts as well as any newly added one...
-  contactsRef.on('child_changed', function(snap) {
-    console.log('updated', snap.key, snap.val());
-    var val = snap.val();
-    // Different than our record
-    if (val.time !== contactsList[snap.key].value.time) {
-      contactsList[snap.key]['value'] = snap.val();
-      addToBucket(snap.key); 
-    }    
-  });
-
-  function updateContact(key, time) {
-    if (time !== contactsList[key]['value'].time) {
-      contactsList[key]['value'].time = time;
-      firebase.database().ref(key).set(contactsList[key]['value']);
-    }
-  }
-
-  // move the html around
-  function addToBucket(key) {
-    // Already has time
-    if (contactsList[key].value.time) {
-      $('[data-time="' + contactsList[key].value.time + '"]').append(contactsList[key].html);
-    } else {
-      contactsList[key].value.time = 0;
-      $('#bucket').append(contactsList[key].html);
-    }
-  }
-
-  //prepare conatct object's HTML
-  function contactFromObject(contact, key){
-    console.log( contact );
-    var html = '';
-    html += '<div class="person">';
-      html += '<span class="handle">+</span>';
-      html += '<span>'+contact.name+'</span>';
-    html += '</div>';
-    return $(html).data({ 'key': key });
-  }
-
   $(window).load(function() {
+
+    // Setting up draggables
+    // -----------------------
 
     var drake = dragula([$('#bucket')[0]], { 
       // moves: function (el, container, handle) {
@@ -81,7 +18,7 @@ firebase.auth().signInAnonymously().catch(function(error) {
       }
     })
     .on('drop', function (el, container) {
-      console.log($(el).data('key'), $(container).data('time'));
+      // console.log($(el).data('key'), $(container).data('time'));
       updateContact($(el).data('key'), $(container).data('time'));
     });
 
@@ -104,7 +41,7 @@ firebase.auth().signInAnonymously().catch(function(error) {
       },
       { 
         time: '5:15 - 7:30',
-        message: 'Dinner',
+        message: 'Dinner and Toasts',
         disabled: true
       },
       { 
@@ -183,8 +120,94 @@ firebase.auth().signInAnonymously().catch(function(error) {
         drake.containers.push($container[0]);
       }
     });
-    
 
+    // Firebase
+    // -----------------------
+
+    var config = {
+      apiKey: "AIzaSyAQxcxbNChO21inp1SKnHfRkY5EEfFs-s0",
+      authDomain: "bartenders-844fa.firebaseapp.com",
+      databaseURL: "https://bartenders-844fa.firebaseio.com",
+      projectId: "bartenders-844fa",
+      storageBucket: "bartenders-844fa.appspot.com",
+      messagingSenderId: "207417592709"
+    };
+    firebase.initializeApp(config);
+
+    console.log('app done');
+
+    // List
+    var contactsList = {};
+
+    //create firebase reference
+    var contactsRef;
+
+    firebase.auth().signInAnonymously()
+      .then(function() {
+        console.log('signed in');
+        contactsRef = firebase.database().ref();
+
+        //load older conatcts as well as any newly added one...
+        contactsRef.on('child_added', function(snap) {
+          console.log('added', snap.key, snap.val());
+          var val = snap.val();
+          contactsList[snap.key] = {
+            value: val,
+            html: contactFromObject(val, snap.key)
+          };
+          addToBucket(snap.key);
+        });
+
+        //load older conatcts as well as any newly added one...
+        contactsRef.on('child_changed', function(snap) {
+          console.log('updated', snap.key, snap.val());
+          var val = snap.val();
+          // Different than our record
+          if (val.time !== contactsList[snap.key].value.time) {
+            contactsList[snap.key]['value'] = snap.val();
+            addToBucket(snap.key); 
+          }    
+        });
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(error);
+      }
+    );
+
+    // Helper funcs
+    // -----------------------
+
+    function updateContact(key, time) {
+      if (time !== contactsList[key]['value'].time) {
+        contactsList[key]['value'].time = time;
+        firebase.database().ref(key).set(contactsList[key]['value']);
+      }
+    }
+
+    // move the html around
+    function addToBucket(key) {
+      // Already has time
+      if (contactsList[key].value.time) {
+        $('[data-time="' + contactsList[key].value.time + '"]').append(contactsList[key].html);
+      } else {
+        contactsList[key].value.time = 0;
+        $('#bucket').append(contactsList[key].html);
+      }
+    }
+
+    //prepare conatct object's HTML
+    function contactFromObject(contact, key){
+      console.log( contact );
+      var html = '';
+      html += '<div class="person">';
+        html += '<span class="handle">+</span>';
+        html += '<span>'+contact.name+'</span>';
+      html += '</div>';
+      return $(html).data({ 'key': key });
+    }
   });
 
 }(jQuery));
